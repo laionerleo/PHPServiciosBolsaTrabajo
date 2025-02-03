ac28fb07442b9ef8358aa6e35c69c037c3ef050e
@leonardoayala <33144217+laionerleo@users.noreply.github.com>
2025-02-02 21:08:14 -0400

    cambiadno que traiga ams detalles de emple

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -17,7 +17,7 @@ class Controller extends BaseController
     {
         try {
             // Realizar la consulta con las relaciones
-            $empleos = DB::table('empleos as e')
+            $empleos = DB::table('empleo as e')
                 ->leftJoin('categoria as c', 'e.Categoria', '=', 'c.Categoria')
                 ->leftJoin('empresa as emp', 'e.Empresa', '=', 'emp.Empresa')
                 ->leftJoin('tipoempleo as te', 'e.TipoEmpleo', '=', 'te.TipoEmpleo')
@@ -55,74 +55,97 @@ class Controller extends BaseController
     }
 
     public function getListaCategorias(Request $request)
-{
-    try {
-        // Realizar la consulta para obtener todas las categorías
-        $categorias = DB::table('categoria')->select('Categoria', 'Nombre')->get();
+    {
+        try {
+            // Realizar la consulta para obtener todas las categorías
+            $categorias = DB::table('categoria')->select('Categoria', 'Nombre')->get();
 
-        // Armar la respuesta
-        $oPaquete = [
-            'error' => true,
-            'message' => 'Categorías obtenidas con éxito.',
-            'values' => $categorias
-        ];
-    }
-    catch (\Throwable $ex) {
-        // Manejo de excepciones
-        $oPaquete = [
-            'error' => false,
-            'message' => $ex->getMessage(),
-            'values' => null
-        ];
-    }
+            // Armar la respuesta
+            $oPaquete = [
+                'error' => true,
+                'message' => 'Categorías obtenidas con éxito.',
+                'values' => $categorias
+            ];
+        }
+        catch (\Throwable $ex) {
+            // Manejo de excepciones
+            $oPaquete = [
+                'error' => false,
+                'message' => $ex->getMessage(),
+                'values' => null
+            ];
+        }
 
-    // Retornar la respuesta en formato JSON
-    return response()->json($oPaquete);
-}
-
-
-public function getDetalleEmpleo(Request $request)
-{
-
-    $tcCodigoEmpleo = $request->input('tcCodigoEmpleo');
-    try {
-        // Realizar la consulta con las relaciones
-        $empleos = DB::table('empleos as e')
-            ->leftJoin('categoria as c', 'e.Categoria', '=', 'c.Categoria')
-            ->leftJoin('empresa as emp', 'e.Empresa', '=', 'emp.Empresa')
-            ->leftJoin('tipoempleo as te', 'e.TipoEmpleo', '=', 'te.TipoEmpleo')
-            ->leftJoin('tiempoexperiencia as tec', 'e.TiempoExperiencia', '=', 'tec.TiempoExperiencia')
-            ->select(
-                'e.Empleo', 'e.Titulo', 'e.Descripcion', 'e.FechaVencimiento', 'e.SalarioAproximado', 
-                'e.FechaPublicacion', 'e.Ubicacion', 'e.Lat', 'e.Lng', 
-                'e.Categoria', 'e.TiempoExperiencia', 
-                'c.Nombre as CategoriaNombre', 
-                'emp.Nombre as EmpresaNombre', 'emp.Descripcion as EmpresaDescripcion',
-                'te.Nombre as TipoEmpleoNombre', 'tec.Titulo as TiempoExperienciaTitulo'
-            )
-            ->where("e.CodigoEmpleo", $tcCodigoEmpleo)
-
-            ->get();
-
-        // Armar la respuesta
-        $oPaquete = [
-            'error' => true,
-            'message' => 'Empleos obtenidos con éxito.',
-            'values' => $empleos
-        ];
-    }
-    catch (\Throwable $ex) {
-        // Manejo de excepciones
-        $oPaquete = [
-            'error' => false,
-            'message' => $ex->getMessage(),
-            'values' => null
-        ];
+        // Retornar la respuesta en formato JSON
+        return response()->json($oPaquete);
     }
 
-    // Retornar la respuesta en formato JSON
-    return response()->json($oPaquete);
-}
+
+    public function getDetalleEmpleo(Request $request)
+    {
+
+        $tcCodigoEmpleo = $request->input('tcCodigoEmpleo');
+        try {
+            // Realizar la consulta con las relaciones
+            $empleos = DB::table('empleo as e')
+                ->leftJoin('categoria as c', 'e.Categoria', '=', 'c.Categoria')
+                ->leftJoin('empresa as emp', 'e.Empresa', '=', 'emp.Empresa')
+                ->leftJoin('tipoempleo as te', 'e.TipoEmpleo', '=', 'te.TipoEmpleo')
+                ->leftJoin('tiempoexperiencia as tec', 'e.TiempoExperiencia', '=', 'tec.TiempoExperiencia')
+                ->select(
+                    'e.Empleo', 'e.Titulo',   'e.DescripcionLarga', 'e.Descripcion', 'e.FechaVencimiento', 'e.SalarioAproximado', 
+                    'e.FechaPublicacion', 'e.Ubicacion', 'e.Lat', 'e.Lng', 
+                    'e.Categoria', 'e.TiempoExperiencia', 
+                    'c.Nombre as CategoriaNombre', 
+                    'emp.Nombre as EmpresaNombre', 'emp.Descripcion as EmpresaDescripcion',
+                    'te.Nombre as TipoEmpleoNombre', 'tec.Titulo as TiempoExperienciaTitulo'
+                )
+                ->where("e.CodigoEmpleo", $tcCodigoEmpleo)
+
+                ->get();
+
+                // traer requerimiento 
+                $empleosrequerimiento = DB::table('empleo as e')
+                ->leftJoin('empleorequerimiento as er', 'e.Empleo', '=', 'er.Empleo')
+                ->select(
+                    'e.Empleo', 'er.Serial', 'er.Descripcion'
+                )
+                ->where("e.CodigoEmpleo", $tcCodigoEmpleo)
+                ->get();
+
+                //traer habilidades
+                $empleoresponsabilidades = DB::table('empleo as e')
+                ->leftJoin('empleoresponsabilidades as er', 'e.Empleo', '=', 'er.Empleo')
+                ->select(
+                    'e.Empleo', 'er.Serial', 'er.Descripcion'
+                )
+                ->where("e.CodigoEmpleo", $tcCodigoEmpleo)
+                ->get();
+
+            $laDatosEmpleo=[
+                "loEmpleo"=> $empleos[0],
+                "laEmpleoRequerimiento"=>$empleosrequerimiento ,
+                "laEmpleoResponsabilidades"=> $empleoresponsabilidades,
+            ] ;
+            // Armar la respuesta
+            $oPaquete = [
+                'error' => true,
+                'message' => 'Empleos obtenidos con éxito.',
+                'values' => $laDatosEmpleo
+            ];
+        }
+        catch (\Throwable $ex) {
+            // Manejo de excepciones
+            $oPaquete = [
+                'error' => false,
+                'message' => $ex->getMessage(),
+                'values' => null
+            ];
+        }
+
+        // Retornar la respuesta en formato JSON
+        return response()->json($oPaquete);
+    }
 
     
 
