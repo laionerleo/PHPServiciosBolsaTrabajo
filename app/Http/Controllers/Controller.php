04d478efbdf81b69b4f4ab344d612699c673de51
@leonardoayala <33144217+laionerleo@users.noreply.github.com>
2025-02-04 00:14:06 -0400

    04-02-2024

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -148,5 +148,127 @@ class Controller extends BaseController
     }
 
     
+    public function getDetalleEmpresa(Request $request)
+    {
+
+        $tnEmpresa = $request->input('tnEmpresa');
+        try {
+            // Realizar la consulta con las relaciones
+            $loEmpresa = DB::table('Empresa as e')
+                            ->join('TipoEmpresa as te', 'e.TipoEmpresa', '=', 'te.id')
+                            ->join('TamañoEmpresa as tme', 'e.TamañoEmpresa', '=', 'tme.id')
+                            ->select(
+                                'e.Nombre',
+                                'e.NombreComercial',
+                                'e.Direccion',
+                                'e.Descripcion',
+                                'e.UrlImagen',
+                                'e.UrlIcono',
+                                'e.Estado',
+                                'te.descripcion as TipoEmpresaDescripcion',
+                                'tme.descripcion as TamañoEmpresaDescripcion',
+                                'e.AñoFundacion'
+                            )
+                            ->where('e.id', '=', $empresaId)
+                            ->first(); // Usamos 'first' porque estamos esperando un solo resultado
+
+
+
+                // traer requerimiento 
+            
+
+            $laDatosEmpresa=[
+                "loEmpresa"=> $loEmpresa,
+               // "laEmpleoRequerimiento"=>$empleosrequerimiento ,
+                //"laEmpleoResponsabilidades"=> $empleoresponsabilidades,
+            ] ;
+            // Armar la respuesta
+            $oPaquete = [
+                'error' => true,
+                'message' => 'Empresa  obtenidos con éxito.',
+                'values' => $laDatosEmpleo
+            ];
+        }
+        catch (\Throwable $ex) {
+            // Manejo de excepciones
+            $oPaquete = [
+                'error' => false,
+                'message' => $ex->getMessage(),
+                'values' => null
+            ];
+        }
+
+        // Retornar la respuesta en formato JSON
+        return response()->json($oPaquete);
+    }
+
+
+    
+    public function getDetalleCandidato(Request $request)
+    {
+
+        $tnCandidato = $request->input('tnCandidato');
+        try {
+            // Realizar la consulta con las relaciones
+            $empleos = DB::table('empleo as e')
+                ->leftJoin('categoria as c', 'e.Categoria', '=', 'c.Categoria')
+                ->leftJoin('empresa as emp', 'e.Empresa', '=', 'emp.Empresa')
+                ->leftJoin('tipoempleo as te', 'e.TipoEmpleo', '=', 'te.TipoEmpleo')
+                ->leftJoin('tiempoexperiencia as tec', 'e.TiempoExperiencia', '=', 'tec.TiempoExperiencia')
+                ->select(
+                    'e.Empleo', 'e.Titulo',   'e.DescripcionLarga', 'e.Descripcion', 'e.FechaVencimiento', 'e.SalarioAproximado', 
+                    'e.FechaPublicacion', 'e.Ubicacion', 'e.Lat', 'e.Lng', 
+                    'e.Categoria', 'e.TiempoExperiencia', 
+                    'c.Nombre as CategoriaNombre', 
+                    'emp.Nombre as EmpresaNombre', 'emp.Descripcion as EmpresaDescripcion',
+                    'te.Nombre as TipoEmpleoNombre', 'tec.Titulo as TiempoExperienciaTitulo'
+                )
+                ->where("e.CodigoEmpleo", $tcCodigoEmpleo)
+
+                ->get();
+
+                // traer requerimiento 
+                $empleosrequerimiento = DB::table('empleo as e')
+                ->leftJoin('empleorequerimiento as er', 'e.Empleo', '=', 'er.Empleo')
+                ->select(
+                    'e.Empleo', 'er.Serial', 'er.Descripcion'
+                )
+                ->where("e.CodigoEmpleo", $tcCodigoEmpleo)
+                ->get();
+
+                //traer habilidades
+                $empleoresponsabilidades = DB::table('empleo as e')
+                ->leftJoin('empleoresponsabilidades as er', 'e.Empleo', '=', 'er.Empleo')
+                ->select(
+                    'e.Empleo', 'er.Serial', 'er.Descripcion'
+                )
+                ->where("e.CodigoEmpleo", $tcCodigoEmpleo)
+                ->get();
+
+            $laDatosEmpleo=[
+                "loEmpleo"=> $empleos[0],
+                "laEmpleoRequerimiento"=>$empleosrequerimiento ,
+                "laEmpleoResponsabilidades"=> $empleoresponsabilidades,
+            ] ;
+            // Armar la respuesta
+            $oPaquete = [
+                'error' => true,
+                'message' => 'Empleos obtenidos con éxito.',
+                'values' => $laDatosEmpleo
+            ];
+        }
+        catch (\Throwable $ex) {
+            // Manejo de excepciones
+            $oPaquete = [
+                'error' => false,
+                'message' => $ex->getMessage(),
+                'values' => null
+            ];
+        }
+
+        // Retornar la respuesta en formato JSON
+        return response()->json($oPaquete);
+    }
+    
 
 }
